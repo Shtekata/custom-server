e22ab0bef3dd95082219ffa4f1591c9f2a27b1ef
Shtekata <gesheval@gmail.com>
2021-03-10 15:59:23 +0200

    Home controller

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,22 +6,7 @@ import { TITLE_SERVER } from '../config/constants.js';
 const router = Router();
 
 router.get('/', (req, res, next) => {
-    if (res.locals.user) {
-        return entityService.getAllDesc(req.query.search)
-        .then(x => {
-            x.forEach(x => x.usersLiked = x.usersLiked.length);
-            res.render('home/home', { title: TITLE_SERVER, plays: x, user: res.locals.user })
-        })
-        .catch(next);
-    }
-    entityService.getAllLikesDesc(req.query.search)
-        .then(x => {
-            x.forEach(y => y.usersLiked = y.usersLiked.length);
-            x.sort((x, y) => y.usersLiked - x.usersLiked);
-            x = x.slice(0, 3);
-            res.render('home/home', { title: TITLE_SERVER, plays: x, user: res.locals.user })
-        })
-        .catch(next);
+    res.json({ message: 'It\'s working!' });
 });
 
 export default router;

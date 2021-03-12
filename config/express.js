7c7aa6639723142039431a6ad0fabfdc444e2a75
Shtekata <gesheval@gmail.com>
2021-03-12 09:01:13 +0200

    Update express.js

--- config/express.js
+++ config/express.js
@@ -14,7 +14,7 @@ function setupExpress(app) {
     app.use(cookieParser());
     app.use(session({ secret: 'shtekata', cookie: { secure: false, maxAge: 1800000 }, resave: false, saveUninitialized: true }));
     app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', 'https://tranquil-sea-17355.herokuapp.com');
+        res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         next()
     });
